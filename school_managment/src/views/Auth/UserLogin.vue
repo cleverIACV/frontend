ab29e518a803bee0ef5b63df484132c0a4b1592f
Frederic Zai <frederic.zai@gmail.com>
2024-08-02 18:12:42 +0200

    add login and register identifiant

--- school_managment/src/views/Auth/UserLogin.vue
+++ school_managment/src/views/Auth/UserLogin.vue
@@ -1,26 +1,43 @@
 <template>
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img
-        class="mx-auto h-10 w-auto"
-        src="../../../public/logo-cv.png"
-        alt="Your Company"
-      />
-      <h2
-        class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
-        v-if="!isAuthenticated"
-      >
-        Connectez-vous à votre compte
-      </h2>
-      <h2
-        class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
-        v-else
-      >
-        Bienvenue
-      </h2>
-    </div>
+    <div class="sm:mx-auto sm:w-full sm:max-w-4xl flex flex-col md:flex-row gap-6">
+      <!-- Formulaires -->
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          class="mx-auto h-10 w-auto"
+          src="../../../public/logo-cv.png"
+          alt="Your Company"
+        />
+        <h2
+          class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+          v-if="!isAuthenticated"
+        >
+          Connectez-vous à votre compte
+        </h2>
+        <h2
+          class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+          v-else
+        >
+          Bienvenue
+        </h2>
+        <UserLoginForm />
+      </div>
 
-    <UserLoginForm />
+      <!-- Identifiants par défaut -->
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm flex flex-col items-start justify-center">
+        <h2
+          class="mt-10 text-left text-2xl font-bold leading-9 tracking-tight text-gray-900"
+        >
+          Identifiants par défaut
+        </h2>
+        <ul class="mt-6 list-disc list-inside text-gray-700 text-left">
+          <li><strong>Email recruiter:</strong> admin2@admin1.com</li>
+          <li><strong>Mot de passe:</strong> 123456</li>
+          <li><strong>Email candidat:</strong> candidat500@test.com</li>
+          <li><strong>Mot de passe Admin:</strong> 123456</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
